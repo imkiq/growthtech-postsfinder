0a2b4046e343c0166a1cec905fc4475d101c8772
kiq <57597713+imkiq@users.noreply.github.com>
2021-01-25 04:06:15 -0300

    timeout fix

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL_dev = 'http://localhost:3000';
-const baseURL_production = 'https://growthtech-postsfinder.herokuapp.com/';
+const baseURL_production = '';
 
 const api = axios.create({
     baseURL: baseURL_production
